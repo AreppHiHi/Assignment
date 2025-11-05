1d7fb56c01c46e4146146b56841432afc02689e8
AreppHiHi <ariffzakwan64@gmail.com>
2025-11-05 20:02:30 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -214,7 +214,7 @@ if st.button("Run All 3 Trials"):
             rows.append({
                 "Time Slot": f"{time_slots[idx]:02d}:00",
                 "Program": program,
-                "Rating": round(rating, 3)
+                "Rating": round(rating, 1)
             })
 
         df_out = pd.DataFrame(rows)
